7fef29e498d5c1b6e115eb94260bd737a343e1e2
Malcolm Daigle <daiglemalcolm@gmail.com>
2016-11-17 09:44:55 -0800

    Adds node dependencies. Removes extra loop.

--- viterbi.js
+++ viterbi.js
@@ -53,10 +53,28 @@ gbb_contents.forEach(function(line){
     }
 });*/
 
-for (var i = 1; i < 11; i++) {
+var diff;
+var time = process.hrtime();
+
+for (var i = 1; i <= 10; i++) {
+    if (i == 9) {
+        diff = process.hrtime(time);
+    }
+
     console.log("Starting round " + i);
-    console.log("Emission Parameters:\n" + emissions);
-    console.log("Transition Parameters:\n" + transitions);
+    console.log();
+    console.log("Emission Parameters:");
+    console.log("State 1:");
+    console.log(emissions.state1);
+    console.log("State 2:");
+    console.log(emissions.state2);
+    console.log();
+    console.log("Transition Parameters:");
+    console.log("From State 1:");
+    console.log(transitions.state1);
+    console.log("From State 2:");
+    console.log(transitions.state2);
+    console.log();
 
     var t_count = {
         "state1": { "state1": 0, "state2": 0 },
@@ -70,13 +88,11 @@ for (var i = 1; i < 11; i++) {
 
     var viterbi_path = getViterbiPath(s_count, t_count, n_count);
 
-    if (i == 11) {
+    if (i == 10) {
         printPathInfo(viterbi_path, -1);
     } else {
         printPathInfo(viterbi_path, 5);
     }
-
-    console.log("Setting new probs");
 
     var total_state_1_transitions = t_count['state1']['state1'] + t_count['state1']['state2'];
 
@@ -91,7 +107,7 @@ for (var i = 1; i < 11; i++) {
 
     transitions['begin']['state2'] = s_count['state2'] / (s_count['state1'] + s_count['state2']);
 
-    // Set transision probs
+    // Set transition probs
     transitions['state1']['state1'] = t_count['state1']['state1'] / total_state_1_transitions;
     transitions['state1']['state2'] = t_count['state1']['state2'] / total_state_1_transitions;
     transitions['state2']['state1'] = t_count['state2']['state1'] / total_state_2_transitions;
@@ -107,11 +123,17 @@ for (var i = 1; i < 11; i++) {
     emissions['state2']['C'] = n_count['state2']['C'] / total_state_2_emissions;
     emissions['state2']['G'] = n_count['state2']['G'] / total_state_2_emissions;
     emissions['state2']['T'] = n_count['state2']['T'] / total_state_2_emissions;
-    console.log("Set the new probs");
-    console.log(i);
+
+    console.log("\n\n");
 }
 
-// TODO: use log probabilities
+console.log("Node 5.10.1");
+console.log("Time: " + (diff[0] + 1e-9 * diff[1]) + " seconds");
+console.log("Device: MacBook Pro (Retina, 13-inch, Late 2013)");
+console.log("Processor: 2.4 GHz Intel Core i5");
+console.log("Memory: 8 GB 1600 MHz DDR3");
+
+
 
 function getViterbiPath(state_count, transition_count, nucleotide_count) {
     var model = [
@@ -139,126 +161,141 @@ function getViterbiPath(state_count, transition_count, nucleotide_count) {
         model[model_index] = {};
 
         // Get probabilities of previous states
-        var prev_state_1 = model[model_index - 1]['state1'];
-        var prev_state_2 = model[model_index - 1]['state2'];
+        var curr_state_1 = model[model_index - 1]['state1'];
+        var curr_state_2 = model[model_index - 1]['state2'];
 
         // Get the emission probability for nucleotide for each state
         var emission_state_1 = emissions['state1'][nucleotide];
         var emission_state_2 = emissions['state2'][nucleotide];
 
-        // Calculate new state 1 probabilities
-        var state_1_to_state_1 = prev_state_1 + Math.log(transitions['state1']['state1']) + Math.log(emission_state_1);
-        var state_2_to_state_1 = prev_state_2 + Math.log(transitions['state2']['state1']) + Math.log(emission_state_1);
+        if (i == 0) {
+            model[model_index]['state1'] = curr_state_1 + Math.log(emission_state_1);
+            model[model_index]['state2'] = curr_state_2 + Math.log(emission_state_2);
+        } else {
+            // Calculate new state 1 probabilities
+            var state_1_to_state_1 = curr_state_1 + Math.log(transitions['state1']['state1']) + Math.log(emission_state_1);
+            var state_2_to_state_1 = curr_state_2 + Math.log(transitions['state2']['state1']) + Math.log(emission_state_1);
 
-        // Set the definitive state 1 probability to the maximum
-        model[model_index]['state1'] = Math.max(state_1_to_state_1, state_2_to_state_1);
+            // Set the definitive state 1 probability to the maximum
+            model[model_index]['state1'] = Math.max(state_1_to_state_1, state_2_to_state_1);
 
-        // Calculate new state 2 probabilities
-        var state_1_to_state_2 = prev_state_1 + Math.log(transitions['state1']['state2']) + Math.log(emission_state_2);
-        var state_2_to_state_2 = prev_state_2 + Math.log(transitions['state2']['state2']) + Math.log(emission_state_2);
+            // Calculate new state 2 probabilities
+            var state_1_to_state_2 = curr_state_1 + Math.log(transitions['state1']['state2']) + Math.log(emission_state_2);
+            var state_2_to_state_2 = curr_state_2 + Math.log(transitions['state2']['state2']) + Math.log(emission_state_2);
 
-        // Set the definitive state 2 probability to the maximum
-        model[model_index]['state2'] = Math.max(state_1_to_state_2, state_2_to_state_2);
+            // Set the definitive state 2 probability to the maximum
+            model[model_index]['state2'] = Math.max(state_1_to_state_2, state_2_to_state_2);
+        }
     }
-
-    //console.log(model);
 
     // Traceback to get path
     var path = [];
 
-    for (var i = fasta_contents.length - 1; i >= 0; i--) {
-        var model_index = i + 1;
-
-        if (model_index == fasta_contents.length) {
-            if (model[model_index]['state1'] >= model[model_index]['state2']) {
-                console.log("Path Log Probability: " + model[model_index]['state1']);
-                path[i] = 'state1';
-                state_count['state1']++;
-            } else {
-                console.log("Path Log Probability: " + model[model_index]['state2']);
-                path[i] = 'state2';
-                state_count['state2']++;
-            }
-            continue;
-        }
-
+    for (var i = fasta_contents.length; i > 0; i--) {
         var nucleotide = fasta_contents.charAt(i).toUpperCase();
+        var curr_nucleotide = fasta_contents.charAt(i - 1).toUpperCase();
 
         // Replace non ACTG characters
         if (NUCLEOTIDES.indexOf(nucleotide) < 0) {
             nucleotide = "T";
         }
+        // Replace non ACTG characters
+        if (NUCLEOTIDES.indexOf(curr_nucleotide) < 0) {
+            curr_nucleotide = "T";
+        }
+
         /*if (parseInt(nucleotide) < 6) {
             nucleotide = "A";
         } else {
             nucleotide = "C";
         }*/
 
-        // Get probabilities of previous states
-        var prev_state_1 = model[model_index - 1]['state1'];
-        var prev_state_2 = model[model_index - 1]['state2'];
+        if (i == fasta_contents.length) {
+            if (model[i]['state1'] >= model[i]['state2']) {
+                console.log("Path Log Probability: " + model[i]['state1']);
+                path[i] = 'state1';
+                state_count['state1']++;
+                nucleotide_count['state1'][curr_nucleotide]++;
+            } else {
+                console.log("Path Log Probability: " + model[i]['state2']);
+                path[i] = 'state2';
+                state_count['state2']++;
+                nucleotide_count['state1'][curr_nucleotide]++;
+            }
+            continue;
+        }
 
         // Get probabilities of current states
-        var curr_state_1 = model[model_index]['state1'];
-        var curr_state_2 = model[model_index]['state2'];
+        var curr_state_1 = model[i]['state1'];
+        var curr_state_2 = model[i]['state2'];
 
-        // Get the emission probability for nucleotide for each state
+        // Get probabilities of next states
+        var next_state_1 = model[i + 1]['state1'];
+        var next_state_2 = model[i + 1]['state2'];
+
+        // Get the emission probability for nucleotide for next states
         var emission_state_1 = emissions['state1'][nucleotide];
         var emission_state_2 = emissions['state2'][nucleotide];
 
         if (path[i + 1] == 'state1') {
             // Calculate probabilities for state transitions
-            var state_1_to_state_1 = prev_state_1 + Math.log(transitions['state1']['state1']) + Math.log(emission_state_1);
-            var state_2_to_state_1 = prev_state_2 + Math.log(transitions['state2']['state1']) + Math.log(emission_state_1);
+            var state_1_to_state_1 = curr_state_1 + Math.log(transitions['state1']['state1']) + Math.log(emission_state_1);
+            var state_2_to_state_1 = curr_state_2 + Math.log(transitions['state2']['state1']) + Math.log(emission_state_1);
 
-            if (state_1_to_state_1 == curr_state_1) {
+            if (state_1_to_state_1 == next_state_1) {
                 path[i] = 'state1';
                 state_count['state1']++;
                 transition_count['state1']['state1']++;
-            } else if (state_2_to_state_1 == curr_state_1) {
+                nucleotide_count['state1'][curr_nucleotide]++;
+            } else if (state_2_to_state_1 == next_state_1) {
                 path[i] = 'state2';
                 state_count['state2']++;
                 transition_count['state2']['state1']++;
+                nucleotide_count['state2'][curr_nucleotide]++;
             } else {
-                assert(false, "No transition gives the expected probability");
+                assert(false, "No transition to state 1 gives the expected probability at index " + i);
             }
             continue;
         } else {
             // Calculate probabilities for state transitions
-            var state_1_to_state_2 = prev_state_1 + Math.log(transitions['state1']['state2']) + Math.log(emission_state_2);
-            var state_2_to_state_2 = prev_state_2 + Math.log(transitions['state2']['state2']) + Math.log(emission_state_2);
+            var state_1_to_state_2 = curr_state_1 + Math.log(transitions['state1']['state2']) + Math.log(emission_state_2);
+            var state_2_to_state_2 = curr_state_2 + Math.log(transitions['state2']['state2']) + Math.log(emission_state_2);
 
-            if (state_1_to_state_2 == curr_state_2) {
+            if (state_1_to_state_2 == next_state_2) {
                 path[i] = 'state1';
                 state_count['state1']++;
                 transition_count['state1']['state2']++;
-            } else if (state_2_to_state_2 == curr_state_2) {
+                nucleotide_count['state1'][curr_nucleotide]++;
+            } else if (state_2_to_state_2 == next_state_2) {
                 path[i] = 'state2';
                 state_count['state2']++;
                 transition_count['state2']['state2']++;
+                nucleotide_count['state2'][curr_nucleotide]++;
             } else {
-                assert(false, "No transition gives the expected probability");
+                assert(false, "No transition to state 2 gives the expected probability at index " + i + "\n" +
+                        state_1_to_state_2 + " " + state_2_to_state_2 + " " + next_state_2);
             }
             continue;
         }
     }
 
-    // TODO: Maybe do this inside the traceback loop?
-    path.forEach(function(elt, i, arr){
+    path.shift();
+
+    /*path.forEach(function(elt, i, arr){
         var nucleotide = fasta_contents.charAt(i).toUpperCase();
 
         // Replace non ACTG characters
         if (NUCLEOTIDES.indexOf(nucleotide) < 0) {
             nucleotide = "T";
         }
-        /*if (parseInt(nucleotide) < 6) {
-            nucleotide = "A";
-        } else {
-            nucleotide = "C";
-        }*/
+        //if (parseInt(nucleotide) < 6) {
+        //    nucleotide = "A";
+        //} else {
+        //    nucleotide = "C";
+        //}
 
         nucleotide_count[elt][nucleotide]++;
-    });
+    });*/
 
     /*var output = "";
     path.forEach(function(elt, i, array){
